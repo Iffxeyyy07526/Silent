36d7fd1ae7817512f7056cbba6c8a49807276574
Iffxeyyy07526 <151912120+Iffxeyyy07526@users.noreply.github.com>
2026-05-04 03:11:22 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/silensexfiltrator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.silensexfiltrator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Optional: Set a simple layout if you want, or keep it empty
+        setContentView(R.layout.activity_main)
+    }
+}
